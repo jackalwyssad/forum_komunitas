1c7018b5995dcaaf59df2e5b2aada5d4d25e70ec
nuralifah27 <nuralifahanggrn27@gmail.com>
2026-04-21 09:49:53 +0700

    menambahkan footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
 import LandingPage from './pages/LandingPage';
 import ThreadsPage from './pages/ThreadsPage';
@@ -21,51 +22,54 @@ function App() {
         <BrowserRouter>
           <div className="app">
             <Navbar />
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/forum" element={<ThreadsPage />} />
-              <Route path="/threads/:id" element={<ThreadDetailPage />} />
-              <Route path="/categories" element={<CategoriesPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
+            <main className="app-main">
+              <Routes>
+                {/* Public Routes */}
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/forum" element={<ThreadsPage />} />
+                <Route path="/threads/:id" element={<ThreadDetailPage />} />
+                <Route path="/categories" element={<CategoriesPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
 
-              {/* Protected Routes */}
-              <Route
-                path="/threads/create"
-                element={
-                  <ProtectedRoute>
-                    <CreateThreadPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/settings"
-                element={
-                  <ProtectedRoute>
-                    <SettingsPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/notifications"
-                element={
-                  <ProtectedRoute>
-                    <NotificationsPage />
-                  </ProtectedRoute>
-                }
-              />
+                {/* Protected Routes */}
+                <Route
+                  path="/threads/create"
+                  element={
+                    <ProtectedRoute>
+                      <CreateThreadPage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/settings"
+                  element={
+                    <ProtectedRoute>
+                      <SettingsPage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/notifications"
+                  element={
+                    <ProtectedRoute>
+                      <NotificationsPage />
+                    </ProtectedRoute>
+                  }
+                />
 
-              {/* Admin Only Routes */}
-              <Route
-                path="/admin/categories"
-                element={
-                  <ProtectedRoute adminOnly>
-                    <AdminCategoriesPage />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
+                {/* Admin Only Routes */}
+                <Route
+                  path="/admin/categories"
+                  element={
+                    <ProtectedRoute adminOnly>
+                      <AdminCategoriesPage />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+            </main>
+            <Footer />
           </div>
         </BrowserRouter>
       </AuthProvider>
